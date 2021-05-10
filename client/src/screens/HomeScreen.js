182472f00d49a80e3e5830a923510325ac634fc7
hhutku <hhutku.wa@gmail.com>
2021-05-10 16:07:39 -0700

    rating added

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -3,13 +3,13 @@ import products from '../products'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 
-const HomeScreen = ({product}) => {
+const HomeScreen = () => {
   return (
     <>
       <h1>Latest products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product}/>
           </Col>
         ))}
