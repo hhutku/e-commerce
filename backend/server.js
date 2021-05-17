2b01e1182cb6bd4c4e5b58f9eef5267a68b71acb
hhutku <hhutku.wa@gmail.com>
2021-05-16 22:56:32 -0700

    costum error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import {notFound, errorHandler} from './middleware/erroorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -12,8 +13,10 @@ app.get('/',(req,res)=>{
     res.send('api is running..')
 })
 
-
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT= process.env.PORT || 5000
 app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
