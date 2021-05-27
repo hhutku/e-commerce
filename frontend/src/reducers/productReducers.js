81449fc8ba021946568c7471dcc82f31a2c1f513
hhutku <hhutku.wa@gmail.com>
2021-05-26 20:36:41 -0700

    product details reducer&action

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -3,6 +3,9 @@ import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
   
   } from '../constants/productConstants'
 
@@ -18,6 +21,22 @@ import {
        
         }
       case PRODUCT_LIST_FAIL:
+        return { loading: false, error: action.payload }
+      default:
+        return state
+    }
+  }
+
+  export const productDetailsReducer = (
+    state = { product: { reviews: [] } },
+    action
+  ) => {
+    switch (action.type) {
+      case PRODUCT_DETAILS_REQUEST:
+        return { ...state, loading: true }
+      case PRODUCT_DETAILS_SUCCESS:
+        return { loading: false, product: action.payload }
+      case PRODUCT_DETAILS_FAIL:
         return { loading: false, error: action.payload }
       default:
         return state
