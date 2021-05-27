78c357d8483fdcbec1d67d9ae9a20d6b24081866
hhutku <hhutku.wa@gmail.com>
2021-05-26 19:58:13 -0700

    message&loader components added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,4 +1,3 @@
-  
 import express from 'express'
 import asyncHandler from 'express-async-handler'
 const router = express.Router()
@@ -7,26 +6,30 @@ import Product from '../models/productModel.js'
 // @desc Fetch all products
 // @route Get /api/products
 // @access public
-router.get('/', asyncHandler(async (req,res)=>{
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
     const products = await Product.find({})
-
+  
     res.json(products)
-}))
+  })
+)
 
 // @desc Fetch single products
 // @route Get /api/products/:id
 // @access public
-router.get('/:id', asyncHandler(async (req,res)=>{
-    const product =await Product.findById(req.params.id)
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
 
-    if(product){
-        res.json(product)
-    }else{
-
-        res.status(404)
-        throw new Error('product not found')
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404)
+      throw new Error('product not found')
     }
-    
-}))
+  })
+)
 
 export default router
