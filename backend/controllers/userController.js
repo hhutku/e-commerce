79aee002f8e56b0cc2cc357bea75354bec6550b1
hhutku <hhutku.wa@gmail.com>
2021-06-06 20:04:17 -0700

    user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,6 +25,40 @@ const authUser =asyncHandler(async (req,res) => {
 }
 })
 
+// @desc Register a new user
+// @route Post /api/users
+// @access public
+const registerUser =asyncHandler(async (req,res) => {
+   
+    const {name,email,password} = req.body
+   
+    const userExist =await User.findOne({email})
+
+    if(userExist){
+        res.status(400)
+        throw new Error("user already exist")
+    }
+ const user = await User.create({
+     name,
+     email,
+     password
+ })
+
+ if(user){
+     res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email : user.email,
+        isAdmin :user.isAdmin,
+        token : generateToken(user._id)
+
+     })
+ }else{
+     res.status(400)
+     throw new Error("Invalid user data")
+ }
+ })
+
 // @desc Get user profile
 // @route Get /api/users/profile
 // @access private
@@ -33,10 +67,10 @@ const getUserProfile =asyncHandler(async (req,res) => {
   const user = await User.findById(req.user._id)
 if(user){
     res.json({
-        _id: user._id,
-        name: user.name,
-        email : user.email,
-        isAdmin :user.isAdmin,
+         _id: user._id,
+           name: user.name,
+           email : user.email,
+           isAdmin :user.isAdmin,
     })
 
 }else {
@@ -47,4 +81,4 @@ if(user){
  })
 
 
-export {authUser,getUserProfile}
+export {authUser,getUserProfile,registerUser}
